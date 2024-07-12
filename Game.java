aad245e2cea7c7a31c19b4e859faa9d1c7a26823
lehercca <gulatil4388@my.sduhsd.net>
2024-07-11 19:18:26 -0700

    testing again :(

--- Game.java
+++ Game.java
@@ -12,7 +12,6 @@ import javax.imageio.ImageIO;
 
 public class Game extends Canvas implements KeyListener, Runnable {
     private Character player;
-    private int testing;
     private String bgLocation = "bg.png";
     private File bg;
     private BufferedImage backg;
